5c63717570b336ae71ec43b20e5587e197736007
justinozzy <jetoswald@gmail.com>
2023-11-29 19:31:22 -0700

    Scuffed solution - Requires all images to be present but works for now

--- client/src/views/Bikes.js
+++ client/src/views/Bikes.js
@@ -1,5 +1,6 @@
-import './Bikes.css';
+import "./Bikes.css";
 import { React, useState, useEffect } from "react";
+import img from "../img/1.png";
 
 function Bike() {
   const [item, setItem] = useState([]);
@@ -11,8 +12,12 @@ function Bike() {
       .then((response) => response.json())
       .then((item) => setItem(item))
       .finally(() => setLoading(false));
-      
   }, []);
+
+  // Call the getImg function
+  function getImg(dataObj) {
+    return "../img/" + dataObj.b_bike_serial_num + ".png";
+  }
 
   // Need to implement filters still
 
@@ -22,12 +27,25 @@ function Bike() {
         <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
           <div class="-m-1 flex flex-wrap md:-m-2">
             {item.map((dataObj, index) => {
+              let imgPath = getImg(dataObj);
               return (
-                    <div class="flex w-1/3 flex-wrap">
-                      <div class="w-full p-1 md:p-2">
-                        <text alt="gallery" class="block h-full w-full rounded-lg object-cover object-center">{dataObj.b_model}</text>
-                      </div>
+                <div className="flex card w-96 bg-base-100 shadow-xl m-6">
+                  <figure>
+                    <img
+                      src={require("../img/" +
+                        dataObj.b_bike_serial_num +
+                        ".png")}
+                      alt={imgPath}
+                    />
+                  </figure>
+                  <div className="card-body">
+                    <h2 className="card-title">{dataObj.b_model}</h2>
+                    <p>${dataObj.b_price}</p>
+                    <div className="card-actions justify-end">
+                      <button className="btn btn-primary">Buy Now</button>
                     </div>
+                  </div>
+                </div>
               );
             })}
           </div>
