f91113f1a5acaa34113282001c46f13cc2ed5c5f
jstnozzy <jetoswald@gmail.com>
2023-12-04 00:47:45 -0700

    Fixed bug and added orders to page - Changed layout as well

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -3,68 +3,76 @@ import { getCookie } from "../components/getCookie";
 
 function Account() {
   const [user, setUser] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const userId = getCookie();
-  const [userInfo, setUserInfo] = useState(["","","",""])
+  const [userInfo, setUserInfo] = useState(["", "", "", ""]);
+  const [orders, setOrders] = useState([]);
 
   useEffect(() => {
-    setLoading(true);
-    const requestOptions = {
-      method: "GET",
+    const fetchData = async () => {
+      try {
+        const requestOptions = {
+          method: "GET",
+        };
+
+        const response = await fetch(
+          "http://localhost:3030/getUser/" + userId,
+          requestOptions
+        );
+        const data = await response.json();
+        setUser(data);
+
+        if (data.userInfo) {
+          setUserInfo([
+            data.userInfo.c_customer_id,
+            data.userInfo.c_email,
+            data.userInfo.c_name,
+            data.userInfo.c_payment_method,
+          ]);
+        } else {
+          console.log("Not found");
+        }
+
+        if (data.orders) {
+          setOrders(data.orders);
+        }
+      } catch (error) {
+        console.log("ERROR: ", error);
+      } finally {
+        setLoading(false);
+      }
     };
-    
-    fetch("http://localhost:3030/getUser/"+userId, requestOptions)
-      .then((response) => response.json())
-      .then((user) => setUser(user))
-      .catch((error) => console.log("ERROR: ", error))
-      .finally(() => setLoading(false));
 
-    console.log(user)
-    if(user.userInfo)
-      setUserInfo([user.userInfo.c_customer_id,user.userInfo.c_email,user.userInfo.c_name,user.userInfo.c_payment_method])
-    else
-      console.log("Not found")
-
-  }, []);
-
-  const updateInfo =  () => {
-    setLoading(true);
-    const requestOptions = {
-      method: "GET",
-    };
-    fetch("http://localhost:3030/getUser/"+userId, requestOptions)
-    .then((response) => response.json())
-    .then((user) => setUser(user))
-    .catch((error) => console.log("ERROR: ", error))
-    .finally(() => setLoading(false));
-  setUserInfo([user.userInfo.c_customer_id,user.userInfo.c_email,user.userInfo.c_name,user.userInfo.c_payment_method])
-  }
+    fetchData();
+  }, [userId]);
 
   console.log(user);
-  console.log(userId);
+  //console.log(userId);
 
-    return (
-      <div className="Account">
-        <header className="App-header">
-          <h1 className="">Account Info</h1>
-          <br></br>
-          <div className="grid grid-cols-2 gap-16" onLoad={updateInfo}>
-            
-            <label>{"[Customer ID#]:" + userInfo[0]}</label>
-            <label>{"[Email]: " + userInfo[1]}</label>
-            <label>{"[Name]:  " + userInfo[2]}</label>
-            <label>{"[Payment Method]:  " + userInfo[3]}</label>
-          </div>
-          <span className="flex-shrink mx-4 text-gray-400">-----------------------------------------------------------------------------------</span>
-          <br></br>
-          <h1> Orders </h1>
-          <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
-            <div class="-m-1 flex flex-wrap md:-m-2">
-              <div className="hero min-h-screen bg-base-200">
-                <div className="hero-content text-center">
-                  <div className="max-w-md"></div>
-                </div>
-              </div>
+  return (
+    <div className="Account">
+      <header className="App-header">
+        <h1 className="" style={{ position: "absolute", top: 100 }}>
+          Account Info
+        </h1>
+        <br></br>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <div className="grid grid-cols-2 gap-16">
+              <label>{"[Customer ID#]:" + userInfo[0]}</label>
+              <label>{"[Email]: " + userInfo[1]}</label>
+              <label>{"[Name]:  " + userInfo[2]}</label>
+              <label>{"[Payment Method]:  " + userInfo[3]}</label>
+            </div>
+            <span className="flex-shrink mx-4 text-gray-400">
+              -----------------------------------------------------------------------------------
+            </span>
+            <br />
+            <h1> Orders </h1>
+
+            <div class="container mx-auto px-5 py-2 lg:px-32 lg:pt-12">
               <div className="overflow-x-auto w-full">
                 <table className="table table-zebra">
                   {/* head */}
@@ -73,17 +81,28 @@ function Account() {
                       <th></th>
                       <th>Store</th>
                       <th>Status</th>
-                      <th>Items</th>
+                      <th>Payment Info</th>
                     </tr>
                   </thead>
-                  <tbody></tbody>
+                  <tbody>
+                    {Object.values(orders).map((order, index) => (
+                      <tr key={index}>
+                        <td>{index + 1}</td>
+                        <td>{order.store_id}</td>
+                        <td>{order.status}</td>
+                        <td>{order.payment_info}</td>
+                      </tr>
+                    ))}
+                  </tbody>
                 </table>
+                {Object.keys(orders).length === 0 && <p>No orders found.</p>}
               </div>
             </div>
-          </div>
-        </header>
-      </div>
-    );
+          </>
+        )}
+      </header>
+    </div>
+  );
 }
 
 export default Account;
